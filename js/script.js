da0b2c22cac70c3f395e0e82bd332e5cb1b0b2e1
Aleiaco02 <alessandroiacovelli83@gmail.com>
2025-09-04 18:06:24 +0200

    soluzione finale esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,33 +1,21 @@
-// // SELEZIONE
-// const miaLampadaSpenta = document.querySelector(".lampadaSpenta");
-// const miaLampadaAccesa = document.querySelector(".lampadaAccesa");
-// const mioInterruttore = document.querySelector(".interruttore");
 
-// console.log(miaLampadaSpenta,miaLampadaAccesa);
+const interruttore = document.querySelector(".interruttore");
+const lampada = document.querySelector(".lampada");
+let spenta = true
 
-// // dichiaro e assegno la funzione al click del bottone
-// mioInterruttore.addEventListener('click', 
-//     onOff
-// );
-// console.log(mioInterruttore);
+interruttore.addEventListener('click',onOff);
 
 
-// // imposto la funzione
-// function onOff() {
-//     console.log(miaLampadaSpenta.style.display);
-//     if (miaLampadaSpenta.style.display === "block") {
-//         // lampada spenta ... accedere
-//         miaLampadaSpenta.style.display = "none"
-//         miaLampadaAccesa.style.display = "block"
-//     }else{
-//         // lampada accesa... spegnere 
-//         miaLampadaAccesa.style.display = "none"
-//         miaLampadaSpenta.style.display = "block"
-//     }
+function onOff() {
+    if (spenta) {
+        lampada.src = "img/yellow_lamp.png"
+        interruttore.innerText = "Spegni"
+        spenta = false
+    }else{
+        lampada.src="img/white_lamp.png"
+        interruttore.innerText = "Accendi"
+        spenta = true
+    }
+
     
-    
-// }
-// console.log(miaLampadaSpenta.style);
-
-
-
+}
